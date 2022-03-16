fe0b936e2e3f39572493a55ff9985945ac175469
Yurik <Yurik@DESKTOP-QE5ICU1>
2022-03-16 15:14:15 +0200

    Add project files.

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,42 @@
+﻿namespace Matrix
+{
+    public class Matrix
+    {
+        public int[,] Mtrx { get; }
+
+        public Matrix(int rank, int raw)
+        {
+            Mtrx = new int[rank, raw];
+        }
+
+        public void FillMatrix()
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < Mtrx.GetLength(1); i++)
+            {
+                for (int j = 0; j < Mtrx.GetLength(0); j++)
+                {
+                    Mtrx[i, j] = rnd.Next(0, 9);
+                }
+            }
+        }
+
+        public int GetSumOfMatrixMainDiagonal()
+        {
+            int sum = 0;
+
+            for (int i = 0; i < Mtrx.GetLength(1); i++)
+            {
+                for (int j = 0; j < Mtrx.GetLength(0); j++)
+                {
+                    if (i == j)
+                    {
+                        sum += Mtrx[i, j];
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Matrix
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the rank in the matrix: ");
+            var rank = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of raws in the matrix: ");
+            var raw = int.Parse(Console.ReadLine());
+
+            Matrix matrix = new Matrix(rank, raw);
+
+            matrix.FillMatrix();
+
+            for (int i = 0; i < matrix.Mtrx.GetLength(1); i++)
+            {
+                for (int j = 0; j < matrix.Mtrx.GetLength(0); j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    if (i == j)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    }
+                    Console.Write(string.Format("{0} ", matrix.Mtrx[i, j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+            var result = matrix.GetSumOfMatrixMainDiagonal();
+
+            Console.WriteLine("Sum of main diagonal is " + result);
+        }
+    }
+}
